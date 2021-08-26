040e51f16b4bacbaba3753aec87f9d1eadc94ecc
Batuhan Tosyali <52931384+Batucan2601@users.noreply.github.com>
2021-08-26 13:31:33 +0300

    UART updated
    transmits and receives with 1 wire

--- UART.sv
+++ UART.sv
@@ -24,73 +24,193 @@
 // 1 byte start bit =>  8 byte data => 1 byte parity => 1 byte stop bit  
 // 1 byte start_get 1 byte start_send
 
-module UART( input logic clk , reset , [0:10]RX, output logic [0:10] TX  );
+module UART( input logic clk , reset , RX, output logic TX  );
     logic [7:0]data;
     UART_RECV recv(clk , reset , RX , data );
     UART_TRANS trans( clk , reset , data , TX );
     
 endmodule 
-module UART_RECV( input logic clk , reset , [0:10]RX ,  output logic [7:0] byte_data
+module UART_RECV( input logic clk , reset ,RX ,  output logic [7:0]byte_data
     );
-    
-    typedef enum logic[1:0] {S0,S1,S2} statetype;
+    logic[7:0] temp_byte_data;
+    typedef enum logic[0:3] {S_start,S_1,S_2,S_3,S_4,S_5,S_6,S_7,S_8 , S_parity ,S_stop    } statetype; // S_1 through S_8 are the data collection states 
     statetype[1:0] state, nextstate;
     
     always_ff@(posedge clk)
     begin
-        if (reset) state <= S0;
-        else state <= nextstate;
+        if (reset)
+        begin
+            state <= S_start;
+            byte_data <= 8'b00000000;
+        end
+        else 
+        begin
+            state <= nextstate;
+        end
     end
     
     always_comb
         begin
-            case(state)
-            S0:
+            if( state == S_start  )
+            begin
+             if( RX ) // start bit byte_data
                 begin
-                    if( RX[0] ) // parity bit check 
-                        begin
-                            nextstate = S1;
-                        end
-                    else
-                        begin
-                            nextstate = S0; 
-                        end 
-                     
+                    nextstate = S_1; 
                 end
-            S1:
-                begin // xor the data 
-                    if( RX[1] ^ RX[2] ^ RX[3] ^ RX[4] ^ RX[5] ^ RX[6] ^RX[7] ^ RX[8]  == RX[9] ) // if parity check is a success 
-                        begin
-                            byte_data[7:0] = RX[1:8];
-                        end
-                    nextstate = S0;
-                end 
-            endcase
+              else
+                begin
+                   byte_data = 8'b00000000;
+                   nextstate = S_start; 
+                end
+            end
+            // multiple data catch start 
+            else if( state == S_1  )
+            begin
+                temp_byte_data[7] = RX;
+                nextstate = S_2; 
+            end
+            else if( state == S_2  )
+            begin
+                temp_byte_data[6] = RX;
+                nextstate = S_3; 
+            end
+            else if( state == S_3  )
+            begin
+                temp_byte_data[5] = RX;
+                nextstate = S_4; 
+            end
+            else if( state == S_4  )
+            begin
+                temp_byte_data[4] = RX;
+                nextstate = S_5; 
+            end
+            else if( state == S_5  )
+            begin
+                temp_byte_data[3] = RX;
+                nextstate = S_6; 
+            end
+            else if( state == S_6  )
+            begin
+                temp_byte_data[2] = RX;
+                nextstate = S_7; 
+            end
+            else if( state == S_7  )
+            begin
+                temp_byte_data[1] = RX;
+                nextstate = S_8; 
+            end
+            else if( state == S_8  )
+            begin
+                temp_byte_data[0] = RX;
+                nextstate = S_parity; 
+            end
+            // multiple data catch end 
+            else if( state == S_parity  )
+            begin
+                // data check
+                if(temp_byte_data[0] ^ temp_byte_data[1] ^temp_byte_data[2] ^temp_byte_data[3] ^temp_byte_data[4] ^ temp_byte_data[5] ^temp_byte_data[6] ^temp_byte_data[7] == RX )
+                begin
+                    nextstate = S_stop; 
+                end
+                else
+                begin
+                    nextstate = S_start;
+                end
+                
+            end
+            else if( state == S_stop)
+            begin
+                if( RX )
+                begin
+                   byte_data = temp_byte_data;
+                   nextstate = S_start; 
+                end
+            end
         end
 endmodule
 
-module UART_TRANS(input logic clk , reset, [7:0]byte_data , output logic[0:10]TX );
-    typedef enum logic[1:0] {S0,S1,S2} statetype;
-    statetype[1:0] state, nextstate;
+module UART_TRANS(input logic clk , reset, [7:0]byte_data , output logic TX );
+    typedef enum logic[0:3] { S_start , S_0,S_1,S_2, S_3, S_4 , S_5 , S_6 , S_7  , S_parity , S_stop } statetype;
+    statetype state, nextstate;
 
     always_ff@(posedge clk)
     begin
-        if (reset) state <= S0;
-        else state <= nextstate;
+        if (reset) 
+        begin 
+            state <= S_start;
+        end
+        else
+        begin
+            state <= nextstate;
+        end 
     end
-    
     always_comb
         begin
             case(state)
-                S0:
+                S_start:
                     begin
-                        TX[1:8] = byte_data; // data set in 1 thruogh 8 
-                        TX[0] = 1'b1; //start bit 
-                        TX[10] = 1'b1; //stop bit
-                        TX[9] = byte_data[0]^ byte_data[1]^ byte_data[2]^ byte_data[3]^ byte_data[4]^ byte_data[5]^ byte_data[6]^ byte_data[7];//parity bit 
-                        nextstate = S0; 
+                        TX = 1'b1; // start bit
+                        nextstate = S_0;
                     end
-
+                 // data send 
+                 S_0:
+                    begin
+                        TX = byte_data[0];
+                        nextstate = S_1;
+                    end
+                 S_1:
+                    begin
+                        TX = byte_data[1];
+                        nextstate = S_2;
+                    end
+                 S_2:
+                    begin
+                        TX = byte_data[2];
+                        nextstate = S_3;
+                    end
+                 S_3:
+                    begin
+                        TX = byte_data[3];
+                        nextstate = S_4;
+                    end 
+                 S_4:
+                    begin
+                        TX = byte_data[4];
+                        nextstate = S_5;
+                    end
+                 S_5:
+                    begin
+                        TX = byte_data[5];
+                        nextstate = S_6;
+                    end
+                S_6:
+                    begin
+                        TX = byte_data[6];
+                        nextstate = S_7;
+                    end
+                S_7:
+                    begin
+                        TX = byte_data[7];
+                        nextstate = S_parity;
+                    end
+                // end of data send
+                S_parity:
+                    begin
+                        if( byte_data[0] ^ byte_data[1] ^byte_data[2] ^byte_data[3] ^byte_data[4] ^byte_data[5] ^byte_data[6] ^byte_data[7] == 1  )
+                        begin
+                            TX = 1'b1; 
+                        end 
+                        else
+                        begin
+                            TX = 1'b0;
+                        end
+                        nextstate = S_stop;
+                    end 
+                 S_stop:
+                    begin
+                        TX = 1'b1; 
+                        nextstate = S_start;
+                    end 
             endcase
         end 
 endmodule 
